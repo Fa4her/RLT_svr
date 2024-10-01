94c2224aa79e784f5670f91657c1a85ca7119593
Fa4her <a.o.blue@outlook.jp>
2024-10-01 15:20:30 +0900

    Add main.css, Remove send_location関連のリンク など

--- server.js
+++ server.js
@@ -6,14 +6,10 @@ let oshagiri_index = 0;
 
 let server = http.createServer((req, res)=>{
   if (req.url==="/"){
-    let a = 0;
     res.writeHead(200, {"Content-Type": "text/html"});
     res.end(fs.readFileSync("./index.html"));
-  } else if (req.url==="/send_location.js"){
-    res.writeHead(200, {"Content-Type": "text/javascript"});
-    res.end(fs.readFileSync("./send_location.js"));
   }
-}).listen(process.env.PORT || 8000); 
+}).listen(8000 || process.env.PORT); 
 
 let io = socket_io(server);
 
